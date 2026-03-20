764cc036ed0d7b83aeffc1aac3decb14edc293e6
sabiransari786 <mohd2302144@st.jmi.ac.in>
2026-03-20 10:38:27 +0530

    Use CSS-designed mobile navbar toggle icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -134,11 +134,11 @@ function Navbar() {
           aria-label="Toggle menu"
           aria-expanded={isMobileMenuOpen}
         >
-          <img
-            src="/images/menu.png?v=2"
-            alt="Menu"
-            className="navbar__mobile-menu-icon"
-          />
+          <span className={`hamburger ${isMobileMenuOpen ? 'open' : ''}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
         </button>
 
         <nav className={`navbar__nav ${isMobileMenuOpen ? 'navbar__nav--open' : ''}`} aria-label="Main navigation">
